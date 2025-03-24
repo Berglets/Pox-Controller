0a2a865296388639076fe88cbd7e3fe09bdd963d
Berglets <115507485+Berglets@users.noreply.github.com>
2025-03-23 20:19:46 -0700

    Update pox_thing.py

--- pox_thing.py
+++ pox_thing.py
@@ -19,12 +19,16 @@ from pox.lib.addresses import EthAddr, IPAddr # Address types
 import pox.lib.util as poxutil                # Various util functions
 import pox.lib.revent as revent               # Event library
 import pox.lib.recoco as recoco               # Multitasking library
+from pox.lib.packet.arp import arp            # ARP 
+from pox.lib.util import dpid_to_str, str_to_bool
+
 
 # Create a logger for this component
 log = core.getLogger()
 
 # Connection for s1
 connection = None
+h5_is_next_server = True
 
 def _go_up (event):
   log.info("Up Event: Skeleton application ready (to do nothing).")
@@ -43,17 +47,26 @@ class MyComponent (object):
     log.info("Switch has come up " + str(event.connection.dpid))
     global connection 
     connection = event.connection
-    
+
+  # ARP requests
   def _handle_PacketIn (self, event):
     inport = event.port
     packet = event.parsed
     if not packet.parsed:
       log.warning("Ignoring unparsed packet")
       return
-    log.info("stuff" + str(packet))
+  
+    log.debug("Got packet: " + str(packet))
 
+    if packet.type != packet.ARP_TYPE: return
+    if packet.payload.opcode != arp.REQUEST: return
     a = packet.find('arp')
-    if not a: return
+
+    log.debug("%s ARP %s %s => %s", dpid_to_str(dpid),
+      {arp.REQUEST:"request",arp.REPLY:"reply"}.get(a.opcode,
+      'op:%i' % (a.opcode,)), str(a.protosrc), str(a.protodst))
+
+
     
     """
     # One thing at a time...
